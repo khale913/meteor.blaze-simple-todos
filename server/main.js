70f6c8ffb67a8f5dae8eddcacc58b260e60f2305
Kelton Hale <keltonhale33@gmail.com>
2021-11-17 23:35:55 -0700

    initial commit

---
+++ server/main.js
@@ -0,0 +1,18 @@
+import { Meteor } from "meteor/meteor";
+import { TasksCollection } from "/Imports/api/TasksCollection";
+
+const insertTask = (taskText) => TasksCollection.insert({ text: taskText });
+
+Meteor.startup(() => {
+  if (TasksCollection.find().count() === 0) {
+    [
+      "First Task",
+      "Third Task",
+      "Third Task",
+      "Fourth Task",
+      "Fifth Task",
+      "Sixth Task",
+      "Seventh Task",
+    ].forEach(insertTask);
+  }
+});
